a9262c68304288fd413737bbcb7fb2f0216967d7
lconaboy <lukeconaboy@Lukes-MacBook-2.local>
2017-02-21 14:40:18 +0000

    fixed file names

--- counting/count_in_cells.py
+++ counting/count_in_cells.py
@@ -1,64 +1,66 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-### could add a random distribution of the brightnesses
-### will the radii be normally distributed?
+xrange = 400
+yrange = xrange
+data = np.random.rand(xrange, yrange) > 0.99
 
-N = 500 #size of the image
-n = 45 #number of field galaxies
-nclust = 30
+cluster_1_loc = [300, 300]
+cluster_1 = abs(np.random.normal(loc=0.0, scale=0.3, size=(20,20))) < 0.05
+cluster_2_loc = [100, 100]
+cluster_2 = abs(np.random.normal(loc=0.0, scale=0.5, size=(40,40))) < 0.05
 
-field = np.zeros(shape=(N,N)) #generate the empty fields
-cluster = np.zeros(shape=(N,N))
+data[290:310, 290:310] = cluster_1
+data[80:120, 80:120] = cluster_2
 
-gal_pos = N*np.random.random_sample((2,n)) #generate n pairs of random positions, which will define the galaxy positions
+# plt.figure('field')
+# plt.imshow(data, cmap='binary')
+# plt.grid()
 
-lmean = 6 #mean radius 
-lsigma = 2 #standard deviation of radius
-l = np.random.normal(lmean, lsigma, n) #a normally distributed range of radii for the galaxies
+def count_in_cells(data, width):
+    #find galaxy clusters by determining the density of galaxies in
+    #small cells and identifying overdensities
 
-for i in range(0,n):
-        
-        #coordinates of the positions of the centre of each galaxy
-        a = gal_pos[0,i]
-        b = gal_pos[1,i]
+    n_cells = int(xrange/width) #number of cells
 
-        #define the indices of the region to place the galaxy
-        pmin = int(max(0, a - 1.25*l[i]))
-        pmax = int(min(N, a + 1.25*l[i]))
-        qmin = int(max(0, b - 1.25*l[i]))
-        qmax = int(min(N, b + 1.25*l[i]))
-        
-        for p in range(pmin,pmax):
-                for q in range(qmin,qmax):
-                    if ((p-a)**2 + (q-b)**2 - l[i]**2) < 1:
-                        field[p,q] = 1*np.exp(-np.sqrt((p-a)**2+(q-b)**2)/l[i]) #the exponential factor is the surface profile
+    data_summed = np.zeros(shape = (n_cells, n_cells))
+    
+    #loop over a grid and sum the data in each 'cell'
+    for i in range(0,n_cells-1):
 
-#define a box for the cluster from clustmin to clustmax in x and y
-clustmin = 200
-clustmax = 230
-clust_pos = clustmax*np.random.random_sample((2,n)) - clustmin
-lclust = np.random.normal(lmean, lsigma, nclust) #a normally distributed range of radii for the galaxies
+        for j in range(0, n_cells-1):
 
+            data_summed[i,j] = np.sum(data[i*width:(i+1)*width, j*width:(j+1)*width])
+    
+    return data_summed
 
-for i in range(0,nclust):
+width = 80 #the width of each cell
 
-        #coordinates of the positions of the centre of each galaxy
-        a = clust_pos[0,i]
-        b = clust_pos[1,i]
-        
-        #define the indices of the region to place the galaxy
-        pmin = int(max(0, a - 1.25*lclust[i]))
-        pmax = int(min(N, a + 1.25*lclust[i]))
-        qmin = int(max(0, b - 1.25*lclust[i]))
-        qmax = int(min(N, b + 1.25*lclust[i]))
-        
-        for p in range(pmin,pmax):
-                for q in range(qmin,qmax):
-                    if ((p-a)**2 + (q-b)**2 - lclust[i]**2) < 1:
-                        cluster[p,q] = 1*np.exp(-np.sqrt((p-a)**2+(q-b)**2)/lclust[i]) #the exponential factor is the surface profile
+data_summed = count_in_cells(data,width)
 
-img = field + cluster
+# The picks out the average points per cell, to be used to cut out background
+vmin = np.sum(data_summed)/(xrange/width)**2
+# etc...
+vmax = np.max(data_summed)
 
-plt.imshow(img, cmap = 'binary')
+plt.figure(1)
+plt.subplot(1, 2, 1)
+plt.imshow(data, cmap='binary')
+plt.subplot(1, 2, 2)
+plt.imshow(data_summed, vmin=vmin, vmax=vmax)
+plt.colorbar()
+plt.grid()
+
+#alternative way to display the counts in cells
+
+#use the standard deviation to give the colourmap some quantiative
+#significance
+sigma = np.std(data_summed)
+data_normed = data_summed/sigma
+
+plt.figure(2)
+plt.imshow(data_normed)
+cax = plt.colorbar()
+cax.set_label('$\sigma$')
+plt.grid()
 plt.show()
